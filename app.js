5c7d6c97007b813b01b333f1266f084f3d4b7869
Geovera <geovera16@gmail.com>
2020-03-22 13:20:22 -0400

    Some code to get you started

---
+++ app.js
@@ -0,0 +1,28 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+let cookieParser = require('cookie-parser');
+let path = require('path');
+let exphbs = require('express-handlebars');
+
+let routes = require('./routes/index');
+//let users = require('./routes/users');
+
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({defaultLayout: 'layout'}));
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+
+app.set('port', (process.env.PORT || 3000));
+
+app.listen(app.get('port'), function(){
+	console.log('Server started on port ' + app.get('port'));
+});
