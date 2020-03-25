b9ea5167629f0d0d5c535ec19e5c0acbfb4f06b0
dtleight <dtleight@gmail.com>
2020-03-25 09:18:00 -0400

    Added function getUser.

--- models/user.js
+++ models/user.js
@@ -1,16 +1,46 @@
-function getUser()
+const express = require("express")
+var mysql = require("mysql")
+const bodyParser = require("body-parser")
+
+const app = express()
+const port = 3000;
+var connection = mysql.createConnection({
+  host: '',
+  user: '',
+  password: '',
+  database: ''
+})
+connection.connect()
+
+function getUser(student)
 {
     //Find user
+    mysql.query(`SELECT * FROM table WHERE id = ${student}`, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    }
+    if (res.length) {
+      console.log("Student: ", res[0]);
+      result(null, res[0]);
+      return;
+    }
+
+    //Student not found
+    result({ kind: "not_found" }, null);
+  });
 }
 
 function searchClassesForUser()
 {
 }
 
+//Alonzo's
 function addCourseToUser()
 {
 }
-
+//Alonzo's
 function removeCourseFromUser()
 {
     
