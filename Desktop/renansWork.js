69a1bb3ffb96617d90cdc3a4a7a18b714fc4ee48
Renan Charles-Pierre <rcharlespierre1@pride.hofstra.edu>
2020-04-04 11:17:15 -0400

    Filter sample code

---
+++ Desktop/renansWork.js
@@ -0,0 +1,14 @@
+//This is where I will work on my stuff
+/* Renan Charles-Pierre
+ *3/22/2020
+ */ 
+
+//Here is some sample code for the project;
+//The Create a dummy array 
+//And use this function to reprint the results of what ever the filter is
+const words = ['element1', 'element2', 'element3', 'element4', 'element5', 'element6'];
+
+const result = words.filter(word => word.length > 6);
+
+console.log(result);
+// expected output: Array ["exuberant", "destruction", "present"]
