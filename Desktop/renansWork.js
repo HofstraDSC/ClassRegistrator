fc5cb6c88f34d68fc154f545add4e2546fd4446d
Renan Charles-Pierre <rcharlespierre1@pride.hofstra.edu>
2020-04-18 23:05:28 -0400

    ear

--- Desktop/renansWork.js
+++ Desktop/renansWork.js
@@ -6,9 +6,18 @@
 //Here is some sample code for the project;
 //The Create a dummy array 
 //And use this function to reprint the results of what ever the filter is
-const words = ['element1', 'element2', 'element3', 'element4', 'element5', 'element6'];
+const words = [
+
+['CourseID', 'CRN', 'Department', 'CourseNumber', 'CourseName', 'CourseDesc', 'Credits', 'ProfessorName ]
+[1, 1234, 'CSC', 15, 'Python', 'Learn_python', 3, 'krishP']
+
+
+
+
+
+
+];
 
 const result = words.filter(word => word.length > 6);
-
 console.log(result);
 // expected output: Array ["exuberant", "destruction", "present"]
