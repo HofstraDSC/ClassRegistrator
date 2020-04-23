874bbc7f3bbbc26db3602509d04f993942666dc2
Alonzo Gonzalez <nyarg1119@aol.com>
2020-04-22 21:32:15 -0400

    Added routes for each function with urls

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,23 @@
 let express = require('express');
 let router = express.Router();
+let user = require('../models/user');
 
 router.get('/', function(req, res){
 	res.render('index');
 });
+
+router.get('/student/:id', user.getUser(req.params.id){
+
+})
+
+router.get('/student/course-list', user.getCoursesfromUser(){
+})
+
+router.post('/student/edit-courses/add', user.addCourseToUser(req.params.id){
+})
+
+router.post('/student/edit-courses/delete', user.removeCourseFromUser(req.params.id, req.params.courseId, req.params.column){
+
+})
 
 module.exports = router;
