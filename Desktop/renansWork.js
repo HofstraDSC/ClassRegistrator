1205e7746f15e597b2498320088b1b79496567cf
Renan Charles-Pierre <rcharlespierre1@pride.hofstra.edu>
2020-04-29 19:45:06 -0400

    Filters

--- Desktop/renansWork.js
+++ Desktop/renansWork.js
@@ -1,14 +1,57 @@
 //This is where I will work on my stuff
 /* Renan Charles-Pierre
  *3/22/2020
- */ 
+ */
+//Sample lists
+test = [
+	['courseID', 'crn', 'department', 'courseNumber', 'courseID', 'courseDescription', 'credits', 'professoName'],
+		['0', '1234', 'CSC', 'Python', 'learn python', '3', 'Krish P'],
+			['1', '3456', 'CSC', 'C++', 'learn c++', '3', 'Simona D'],
+				['2', '7891', 'CSC', 'Java', 'learn java', '3', 'JiangChenS'],
+					['3', '1112', 'CSC', 'Intro to Web Dev', 'Learn the basics of web development', '3']
+];
 
-//Here is some sample code for the project;
-//The Create a dummy array 
-//And use this function to reprint the results of what ever the filter is
-const words = ['element1', 'element2', 'element3', 'element4', 'element5', 'element6'];
+//The filter function
+function readlist(word, list) {
+	wordStr = word.toLowerCase();	
+	for(i =0; i < list.length; i++){
+		listStr = list[i].toString().toLowerCase();
+		if ( listStr == wordStr) {
+			return true;
+		}
+	}
+	return false;
+}
 
-const result = words.filter(word => word.length > 6);
+tan = test.filter(() => readlist('learn', test));	
 
-console.log(result);
-// expected output: Array ["exuberant", "destruction", "present"]
+
+
+
+// Only returns the elements that are true
+
+function readList2D(word, list){
+	arr = [];
+	for(j = 0; j <= readList2D.length; j++){
+		newList = list[j];
+		ans = newList.filter(() => readlist(word, newList));  
+		if (ans.length != 0) {
+			arr += ans;
+			console.log(ans);
+		}
+	}return arr;
+}
+
+
+
+
+ var items = [
+   ['1', '2'],
+     ['3', '4'],
+       ['5', '3']
+       ];
+
+//newItems = items[1];
+//console.log(Array.isArray(newItems));
+
+console.log(readList2D('1', test));
