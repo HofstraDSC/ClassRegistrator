bb0510c3076bd47f4d377463d8ed9c2a0dd948bc
jj3735 <jaysonm3735@gmail.com>
2020-04-29 09:47:56 -0400

    sort of fixed DB
    db.query still does not work

--- models/user.js
+++ models/user.js
@@ -1,4 +1,64 @@
 const db =  require('../models/database');
+
+module.exports = {
+  getUser: function(id){
+    //Requires parameter for user id
+    //Find user
+    db.query(`SELECT * FROM scheduler_data.Student WHERE StudentId = ${id}`, (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(err, null);
+        return;
+      }
+      if (res.length) {
+        result(null, res[0]);
+        return;
+      }
+      //Student not found
+      result({ kind: "not_found" }, null);
+    });
+  },
+
+  searchClassesForUser: function(){
+    //Requires no parameters
+     db.query(`SELECT * FROM scheduler_data.Courses;`, function (err, res) {
+        if (err)
+        {        
+          console.log("error: ", err);
+          result(err, null);
+          return;
+        }
+        result(null,res);
+      });
+  },
+
+  
+  addCourseToUser: function(sid, courseID, column){
+    //Requires parameters for student id, course id and column index
+    db.query(`UPDATE scheduler_data.Students SET {column} = ${courseID} WHERE StudentID = ${sid}`,function (err, res) {
+      if (err)
+      {        
+          console.log("error: ", err);
+          result(err, null);
+          return;
+      }
+    });
+  },
+
+  
+  removeCourseFromUser: function(sid, courseID, column){
+    //Requires parameters for student id, course id and column index
+    db.query(`UPDATE scheduler_data.Students SET ${column} = "" WHERE StudentID = ${sid}`,function (err, res) {
+      if (err)
+      {        
+        console.log("error: ", err);
+        result(err, null);
+        return;
+      }
+    });
+  }
+}
+/*
 //Requires parameter for user id
 function getUser(id)
 {
@@ -53,4 +113,4 @@ function removeCourseFromUser(sid, courseID, column)
         result(err, null);
         return;
     }});
-}
+}*/
